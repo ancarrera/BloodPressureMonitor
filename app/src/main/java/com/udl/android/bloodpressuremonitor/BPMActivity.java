f044ebb525a3e52370be0e8479fddfafd218ac61
ancarrera <ancarrera31@gmail.com>
2015-02-23 18:51:04 +0100

    adding permission/features to manifest

--- app/src/main/java/com/udl/android/bloodpressuremonitor/BPMActivity.java
+++ app/src/main/java/com/udl/android/bloodpressuremonitor/BPMActivity.java
@@ -2,8 +2,6 @@ package com.udl.android.bloodpressuremonitor;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class BPMActivity extends ActionBarActivity {
@@ -14,26 +12,4 @@ public class BPMActivity extends ActionBarActivity {
         setContentView(R.layout.bloodpressuremonitorlayout);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
