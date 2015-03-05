674701c0d7f22817aeb5a475b48adf975402198a
ancarrera <ancarrera31@gmail.com>
2015-03-05 02:00:41 +0100

    working with heart rate

--- app/src/main/java/com/udl/android/bloodpressuremonitor/BPMActivity.java
+++ app/src/main/java/com/udl/android/bloodpressuremonitor/BPMActivity.java
@@ -2,10 +2,15 @@ package com.udl.android.bloodpressuremonitor;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.udl.android.bloodpressuremonitor.application.BPMmasterActivity;
 import com.udl.android.bloodpressuremonitor.interfaces.HomeButtonsListener;
+
+import org.w3c.dom.Text;
 
 
 public class BPMActivity extends BPMmasterActivity
@@ -20,13 +25,17 @@ public class BPMActivity extends BPMmasterActivity
         BLUETOOTH
     }
 
+    private TextView headertextview;
+    private ImageButton buttonbar;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bloodpressuremonitorlayout);
-        configureBaseActionBar();
+        configureActionBar();
         defineListeners();
+        addTypefacesToTextViews();
     }
 
 
@@ -39,8 +48,32 @@ public class BPMActivity extends BPMmasterActivity
 
     }
 
+    private void addTypefacesToTextViews(){
+
+        TextView menu1 = (TextView) findViewById(R.id.heartratetext);
+        TextView menu2 = (TextView) findViewById(R.id.measurementtext);
+        TextView menu3 = (TextView) findViewById(R.id.obtainpressurestext);
+        TextView menu4 = (TextView) findViewById(R.id.addmeditiontext);
+        TextView menu5 = (TextView) findViewById(R.id.perfiltext);
+        TextView menu6 = (TextView) findViewById(R.id.helptext);
+
+        menu1.setTypeface(getMntcorsiva());
+        menu2.setTypeface(getMntcorsiva());
+        menu3.setTypeface(getMntcorsiva());
+        menu4.setTypeface(getMntcorsiva());
+        menu5.setTypeface(getMntcorsiva());
+        menu6.setTypeface(getMntcorsiva());
+
+    }
+
     private void configureActionBar(){
+
         configureBaseActionBar();
+        View view = getActionBarView();
+        headertextview = (TextView) view.findViewById(R.id.textactionbar);
+        headertextview.setText(getResources().getString(R.string.principaltext).toUpperCase());
+        buttonbar = (ImageButton) findViewById(R.id.actionbarbutton);
+        buttonbar.setVisibility(View.GONE);
     }
 
     @Override
