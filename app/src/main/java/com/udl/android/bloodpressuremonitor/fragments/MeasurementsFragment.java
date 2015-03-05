7966b8751f4e162e7b15270c3bb8dab448a03457
ancarrera <ancarrera31@gmail.com>
2015-03-05 03:13:58 +0100

    measurement list

---
+++ app/src/main/java/com/udl/android/bloodpressuremonitor/fragments/MeasurementsFragment.java
@@ -0,0 +1,34 @@
+package com.udl.android.bloodpressuremonitor.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Adrian on 5/3/15.
+ */
+public class MeasurementsFragment extends Fragment {
+
+
+    public static MeasurementsFragment getNewInstance(){
+
+        return new MeasurementsFragment();
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return null;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+
+}
---
+++ app/src/main/java/com/udl/android/bloodpressuremonitor/model/Pressure.java
@@ -0,0 +1,51 @@
+package com.udl.android.bloodpressuremonitor.model;
+
+import java.util.Date;
+
+/**
+ * Created by Adrian on 5/3/15.
+ */
+public class Pressure {
+
+    private String systolic;
+    private String diastolic;
+    private String pulse;
+    private Date date;
+
+    private Pressure(){
+
+
+    }
+
+    public String getSystolic() {
+        return systolic;
+    }
+
+    public void setSystolic(String systolic) {
+        this.systolic = systolic;
+    }
+
+    public String getDiastolic() {
+        return diastolic;
+    }
+
+    public void setDiastolic(String diastolic) {
+        this.diastolic = diastolic;
+    }
+
+    public String getPulse() {
+        return pulse;
+    }
+
+    public void setPulse(String pulse) {
+        this.pulse = pulse;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
